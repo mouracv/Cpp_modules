02284d3870b8a9b8a59cbdda5253d310e9cdaf87
aleperei <aleperei@student.42lisboa.com>
2024-04-05 17:55:12 +0100

    first commit (testando classes)

---
+++ cpp_00/ex01/phonebook.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 14:31:59 by aleperei          #+#    #+#             */
+/*   Updated: 2024/04/05 17:32:16 by aleperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+ #include <iostream>
+ #include <ctype.h>
+ #include <stdlib.h>
+ #include <strings.h>
+ #include <string.h>
+
+
+class Carros
+{
+    public:
+        Carros(int fuel_value, int size, char *name);
+        ~Carros();
+        
+        int fuel;
+        int tamanho;
+        char *marca;
+};
+
+
+Carros::Carros(int fuel_value, int size, char *name)
+    : fuel(fuel_value), tamanho(size), marca(name) //lista de inicializazao
+{
+    std::cout << "Constructor called" << std::endl;
+    return ;
+}
+
+Carros::~Carros()
+{
+    std::cout << "DESTRUCTOR  called" << std::endl;
+    free(this->marca);
+    return ;
+}
+
+
+ int main()
+ {
+    Carros alex(100, 20, strdup("BUGGATI"));
+    
+    std:: cout << alex.marca << " tem um comprimento de " << alex.tamanho << " e tam a capacidade de guardar " << alex.fuel << std::endl;
+    return (0);
+ }
+ 
