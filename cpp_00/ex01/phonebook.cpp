69082096d2b191422ae3c0d609c2fd0c6db494ec
aleperei <aleperei@student.42lisboa.com>
2024-04-08 18:03:01 +0100

    testes 3

--- cpp_00/ex01/phonebook.cpp
+++ cpp_00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 14:31:59 by aleperei          #+#    #+#             */
-/*   Updated: 2024/04/05 17:32:16 by aleperei         ###   ########.fr       */
+/*   Updated: 2024/04/08 16:50:42 by aleperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,32 @@
 
 class Carros
 {
+    private:
+        char *_matricula;
+
+    
     public:
-        Carros(int fuel_value, int size, char *name);
+        Carros(int fuel_value, int size, char *name, char *matricula);
         ~Carros();
         
         int fuel;
         int tamanho;
         char *marca;
-        void   car_print(void);
+        void set_matricula(char *str);
+        char*  get_matricula(void) const;
+        void   car_print(void) const;
 };
 
-void   Carros::car_print(void)
+void   Carros::car_print(void) const
 {
-    std::cout << "metodo chamado kkkk" << std::endl;
+    std::cout << "metodo chamado com sucesso" << std::endl;
 }
 
-Carros::Carros(int fuel_value, int size, char *name)
+Carros::Carros(int fuel_value, int size, char *name, char *matricula)
     : fuel(fuel_value), tamanho(size), marca(name) //lista de inicializazao
 {
     std::cout << "Constructor called" << std::endl;
+    this->set_matricula(matricula);
     return ;
 }
 
@@ -47,16 +54,29 @@ Carros::~Carros()
 {
     std::cout << "DESTRUCTOR  called" << std::endl;
     free(this->marca);
+    free(this->_matricula);
     return ;
+}
+
+void  Carros::set_matricula(char *str)
+{
+    this->_matricula = str;
+    return;
+}
+
+char*  Carros::get_matricula(void) const
+{
+    return(this->_matricula);
 }
 
 
  int main()
  {
-    Carros alex(100, 20, strdup("BUGGATI"));
+    Carros alex(100, 20, strdup("BUGGATI"), strdup("UA-91-EU"));
     
-    std:: cout << alex.marca << " tem um comprimento de " << alex.tamanho << " e tam a capacidade de guardar " << alex.fuel << std::endl;
+    std:: cout << alex.marca << "print 1 tem um comprimento de " << alex.tamanho << " e tam a capacidade de guardar " << alex.fuel << std::endl;
     alex.car_print();
+    std:: cout << "a maticula do carro e " << alex.get_matricula() << std::endl;
     return (0);
  }
  
