3f3556efa7ed42fc5f7649951b4bfc0fc78eafc5
aleperei <aleperei@student.42lisboa.com>
2024-05-23 16:59:22 +0100

    cpp05 folder added

---
+++ cpp05/ex01/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 17:41:01 by aleperei          #+#    #+#             */
+/*   Updated: 2024/05/23 16:57:23 by aleperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+// #include "../include/Animal.hpp"
+// #include "../include/Dog.hpp"
+// #include "../include/Cat.hpp"
+// #include "../include/WrongAnimal.hpp"
+// #include "../include/WrongCat.hpp"
+
+// int main()
+// {
+//     const Animal* meta = new Animal();
+//     const Animal* j = new Dog();
+//     const Animal* i = new Cat();
+//     std::cout << j->getType() << " " << RESET << std::endl;
+//     std::cout << i->getType() << " " << RESET << std::endl;
+//     i->makeSound(); //will output the cat sound!
+//     j->makeSound();
+//     meta->makeSound();
+//     delete j;
+//     delete i;
+//     delete meta;
+
+//     std::cout << std::endl << YELLOW << "WRONG TEST" << RESET << std::endl << std::endl;
+    
+//     const WrongAnimal* alex = new WrongAnimal();
+//     const WrongAnimal* andre = new WrongCat();
+//     std::cout << alex->getType() << " " << RESET << std::endl;
+//     std::cout << andre->getType() << " " << RESET << std::endl;
+//     alex->makeSound();
+//     andre->makeSound();
+
+//     delete alex;
+//     delete andre;
+    
+//     return 0;
+// }
