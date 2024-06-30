b70ca1b839f2c0d2144fcc7309611e5567df639c
mouracv <moreiraalexsandro954@gmail.com>
2024-06-30 19:05:10 +0100

    cpp08 ex02 started

---
+++ cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 18:52:21 by aleperei          #+#    #+#             */
+/*   Updated: 2024/06/30 19:04:28 by aleperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      
+#define GREEN   "\033[32m"      
+#define YELLOW  "\033[33m"      
+#define BLUE    "\033[34m"      
+#define MAGENTA "\033[35m"      
+#define CYAN    "\033[36m" 
+
+
+
+#endif
