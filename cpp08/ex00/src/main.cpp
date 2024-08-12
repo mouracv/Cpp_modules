a1dd4e640c8286ffec5ad4d80f55fd5a87fbe264
aleperei <aleperei@student.42lisboa.com>
2024-08-12 14:59:53 +0100

    ok

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -6,12 +6,42 @@
 /*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 13:42:22 by aleperei          #+#    #+#             */
-/*   Updated: 2024/06/27 16:07:30 by aleperei         ###   ########.fr       */
+/*   Updated: 2024/08/12 12:28:11 by aleperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/easyfind.h"
 
+
+void printDeque(const std::deque<int>& dq) {
+    for (std::deque<int>::const_iterator it = dq.begin(); it != dq.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+void printVector(const std::vector<int>& vec) {
+    for (std::vector<int>::const_iterator it = vec.begin(); it != vec.end(); ++it) {
+        std::cout << *it;
+        if ((it + 1) != vec.end())
+            std::cout << ", ";
+    }
+    std::cout << std::endl;
+}
+
+void print_lista(const std::list<int>& lista) {
+    // Declarando um iterador para percorrer a lista
+    std::list<int>::const_iterator it;
+    std::list<int>::const_iterator next;
+
+    for (it = lista.begin(); it != lista.end(); ++it) {
+        std::cout << *it;
+        next = it;
+        if (++next != lista.end())
+            std::cout << ", ";
+    }
+    std::cout << std::endl;
+}
 
 template<typename T>
 int fill_cont(T& target, size_t len)
@@ -22,7 +52,6 @@ int fill_cont(T& target, size_t len)
     }
     return(rand() % (20 - 0 + 1));
 }
-
 
 template<typename T>
 void    print_cont(T& target, typename T::iterator it, std::string name)
@@ -41,15 +70,23 @@ int main(void)
     
     std::vector<int> alex;
     find = fill_cont(alex, 7);
+    printVector(alex);
+    std::cout << "Number to find " << find << std::endl;
     print_cont(alex, easyfind(alex, find), "Alex");
 
     
     std::list<int> gab;
     find = fill_cont(gab, 10);
+    std::cout << std::endl;
+    print_lista(gab);
+    std::cout << "Number to find " << find << std::endl;
     print_cont(gab, easyfind(gab, find), "Gab");
 
     std::deque<int> leo;
     find = fill_cont(leo, 11);
+    std::cout << std::endl;
+    printDeque(leo);
+    std::cout << "Number to find " << find << std::endl;
     print_cont(leo, easyfind(leo, find), "Leo");
    
     
